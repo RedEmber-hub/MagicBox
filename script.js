0c6b2b4e7d6fdd6051660942416b2c706718575c
RedEmber-hub <redember38@gmail.com>
2025-04-28 15:23:38 +0300

    переписан код, добавлена хэш таблица

--- script.js
+++ script.js
@@ -8,14 +8,20 @@ const rangeSpan = document.getElementById('range-span');
 const zhmyakBtn = document.getElementById('e_btn');
 zhmyakBtn.style.display = 'none';
 
+const colors = new Map([
+    ['yellow', 'yellow'],
+    ['red', 'red'],
+    ['green', 'green'],
+    ['blue', 'blue'],
+    ['pink', 'pink']
+]);
+
 button.addEventListener('click', () => {
     const color = colorInput.value.trim().toLowerCase();
 
-    if (color === 'yellow') square.style.backgroundColor = 'yellow';
-    if (color === 'red') square.style.backgroundColor = 'red';
-    if (color === 'green') square.style.backgroundColor = 'green';
-    if (color === 'blue') square.style.backgroundColor = 'blue';
-    if (color === 'pink') square.style.backgroundColor = 'pink';
+    if (colors.has(color)) { //0, '', null, underfined, NaN - false
+        square.style.backgroundColor = colors.get(color);
+    }
 });
 
 rangeInput.addEventListener('input', () => {
